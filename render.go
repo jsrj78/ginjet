85d85ee36e7034bfa189948120c50d88f67039d3
jsrj78 <jsrj78@users.noreply.github.com>
2021-05-21 23:17:46 -0700

    Update render.go

--- render.go
+++ render.go
@@ -13,15 +13,16 @@ type JetRender struct {
 	Options  *RenderOptions
 	Template *jet.Template
 	Data     interface{}
-	Set      *jet.Set
+	Variables jet.VarMap,
+	globals   jet.VarMap,
 }
 
 // New creates a new JetRender instance with custom Options.
 func New(options *RenderOptions) *JetRender {
-	set := jet.NewHTMLSet(options.TemplateDir)
+	//set := jet.NewHTMLSet(options.TemplateDir)
 	return &JetRender{
 		Options: options,
-		Set:set,
+		//Set:set,
 	}
 }
 
@@ -32,18 +33,60 @@ func Default() *JetRender {
 
 func (r JetRender) Instance(name string, data interface{}) render.Render {
 	set := jet.NewHTMLSet(r.Options.TemplateDir)
+	//设置全局变量
+	if r.globals != nil {
+		for key, value := range r.globals {
+			set.AddGlobal(key, value)
+		}
+	}
+
 	t, err := set.GetTemplate(name)
 
 	if err != nil {
 		panic(err)
 	}
 
+	var v jet.VarMap
+	if data != nil {
+		vars, ok := data.(jet.VarMap)
+		if ok == false {
+
+			varMap, ok := data.(gin.H)
+
+			if !ok {
+				//varMap, err := data.(map[string]interface{})
+				varMap = structs.Map(data) //不是gin.H类型就是map[string]interface{}类型
+			}
+
+			v = make(jet.VarMap)
+
+			for key, value := range varMap {
+				v.Set(key, value)
+			}
+		} else {
+			v = vars
+		}
+	}
+
+
 	return JetRender{
 		Data:     data,
 		Options:  r.Options,
 		Template: t,
-		Set:      set,
+		//Set:      set,
+		Variables: v,
 	}
+}
+
+func (r *JetRender) AddGlobal(key string, i interface{}) {
+	if r.globals == nil {
+		r.globals = make(jet.VarMap)
+	}
+	r.globals[key] = reflect.ValueOf(i)
+}
+
+func (r *JetRender) AddGlobalFunc(key string, fn jet.Func) {
+	r.AddGlobal(key, fn)
 }
 
 func (r JetRender) Render(w http.ResponseWriter) error {
